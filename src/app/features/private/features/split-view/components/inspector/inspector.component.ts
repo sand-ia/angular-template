1d68e7d3bc4603621cd81254859ae75e387a9ad4
mikevive <mikevive327@gmail.com>
2024-02-29 21:48:37 -0500

    refactor(inspector): move inspecotr visibility logic from featureA Component to Inspector Component
    Co-authored-by: waltermayor <waltermayor@gmail.com>

--- src/app/features/private/features/split-view/components/inspector/inspector.component.ts
+++ src/app/features/private/features/split-view/components/inspector/inspector.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-inspector',
   templateUrl: './inspector.component.html',
   styleUrls: ['./inspector.component.scss'],
 })
-export class InspectorComponent {}
+export class InspectorComponent implements OnInit {
+  isInspectorVisible: boolean = false;
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.setInspectorVisibility();
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.setInspectorVisibility());
+  }
+
+  private setInspectorVisibility(): void {
+    this.isInspectorVisible = !!this.route.firstChild;
+  }
+}
--- src/app/features/private/features/split-view/features/feature-a/feature-a.component.ts
+++ src/app/features/private/features/split-view/features/feature-a/feature-a.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feature-a',
   templateUrl: './feature-a.component.html',
   styleUrls: ['./feature-a.component.scss'],
 })
-export class FeatureAComponent implements OnInit {
-  isInspectorVisible: boolean = false;
-  constructor(private router: Router, private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.setInspectorVisibility();
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(() => this.setInspectorVisibility());
-  }
-
-  private setInspectorVisibility(): void {
-    this.isInspectorVisible = !!this.route.firstChild;
-  }
-}
+export class FeatureAComponent {}
