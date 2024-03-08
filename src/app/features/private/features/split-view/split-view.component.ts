8227f8712170d6aa41b2cf485f5b93fadd669a8a
mikevive <mikevive327@gmail.com>
2024-03-07 23:45:51 -0500

    refactor(split-view): clean and organize the code
    Co-authored-by: waltermayor <waltermayor@gmail.com>

--- src/app/features/private/features/split-view/split-view.component.ts
+++ src/app/features/private/features/split-view/split-view.component.ts
@@ -7,9 +7,5 @@ import { SplitViewService } from './split-view.service';
   styleUrls: ['./split-view.component.scss'],
 })
 export class SplitViewComponent {
-  constructor(private splitViewService: SplitViewService) {}
-
-  setActiveCanvas(route: string): void {
-    this.splitViewService.setActiveCanvas(route);
-  }
+  constructor(public splitViewService: SplitViewService) {}
 }
