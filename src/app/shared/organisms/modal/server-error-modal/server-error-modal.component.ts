330cb3ce093f12c8577e22d212325fb8c7b28027
mikevive <mikevive327@gmail.com>
2024-03-18 20:08:26 -0500

    refactor(modal): remove angular material
    Co-authored-by: waltermayor <waltermayor@gmail.com>

--- src/app/shared/organisms/modal/server-error-modal/server-error-modal.component.ts
+++ src/app/shared/organisms/modal/server-error-modal/server-error-modal.component.ts
@@ -1,7 +1,10 @@
+import { DialogRef } from '@angular/cdk/dialog';
 import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './server-error-modal.component.html',
   styleUrls: ['./server-error-modal.component.scss'],
 })
-export class ServerErrorModalComponent {}
+export class ServerErrorModalComponent {
+  constructor(public dialogRef: DialogRef<string>) {}
+}
