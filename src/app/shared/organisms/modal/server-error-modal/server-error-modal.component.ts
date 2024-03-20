050ffa500b93e2dcfe2d60396dfa67b68c090887
mikevive <mikevive327@gmail.com>
2024-03-19 21:47:27 -0500

    Revert "refactor(modal): remove angular material"
    This reverts commit 330cb3ce093f12c8577e22d212325fb8c7b28027.

--- src/app/shared/organisms/modal/server-error-modal/server-error-modal.component.ts
+++ src/app/shared/organisms/modal/server-error-modal/server-error-modal.component.ts
@@ -1,10 +1,7 @@
-import { DialogRef } from '@angular/cdk/dialog';
 import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './server-error-modal.component.html',
   styleUrls: ['./server-error-modal.component.scss'],
 })
-export class ServerErrorModalComponent {
-  constructor(public dialogRef: DialogRef<string>) {}
-}
+export class ServerErrorModalComponent {}
