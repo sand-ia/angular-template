118a2ef68886cd738aff457d9e0d50f922b64890
mikevive <mikevive327@gmail.com>
2024-03-20 23:42:07 -0500

    (refactor): add material dialog
    Co-authored-by: waltermayor <waltermayor@gmail.com>

--- src/app/core/interceptors/server-error.interceptor.ts
+++ src/app/core/interceptors/server-error.interceptor.ts
@@ -8,12 +8,12 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Dialog } from '@angular/cdk/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ServerErrorModalComponent } from '../../shared/organisms/modal/server-error-modal/server-error-modal.component';
 
 @Injectable()
 export class ServerErrorInterceptor implements HttpInterceptor {
-  constructor(private dialog: Dialog) {}
+  constructor(private dialog: MatDialog) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -21,20 +21,13 @@ export class ServerErrorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 0 || (error.status >= 500 && error.status < 599))
-          this.openDialog();
+        if (error.status === 0) {
+          this.dialog.open(ServerErrorModalComponent);
+        } else if (error.status >= 500 && error.status < 599) {
+          this.dialog.open(ServerErrorModalComponent);
+        }
         return throwError(() => error);
       })
     );
-  }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open<string>(ServerErrorModalComponent, {
-      width: '250px',
-    });
-
-    dialogRef.closed.subscribe(() => {
-      console.log('The dialog was closed');
-    });
   }
 }
