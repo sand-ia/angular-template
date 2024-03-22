1dbf8f203613c427517749e1b00c5531a9ae0cfb
mikevive <mikevive327@gmail.com>
2024-03-21 22:39:49 -0500

    feature(modal): allow close modal
    Co-authored-by: waltermayor <waltermayor@gmail.com>

--- src/app/shared/organisms/modal/modal.module.ts
+++ src/app/shared/organisms/modal/modal.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AtomsModule } from '../../atoms/atoms.module';
 import { ServerErrorModalComponent } from './server-error-modal/server-error-modal.component';
+import { MaterialsModule } from '../../materials/materials.module';
 
 @NgModule({
   declarations: [ServerErrorModalComponent],
-  imports: [CommonModule, AtomsModule],
+  imports: [CommonModule, AtomsModule, MaterialsModule],
 })
 export class ModalModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { DialogModule } from '@angular/cdk/dialog';
 import { OrganismsModule } from './organisms/organisms.module';
 import { AtomsModule } from './atoms/atoms.module';
-import { MaterialsModule } from './materials/materials.module';
 
 @NgModule({
-  imports: [
-    ReactiveFormsModule,
-    MaterialsModule,
-    DialogModule,
-    OrganismsModule,
-    AtomsModule,
-  ],
+  imports: [ReactiveFormsModule, DialogModule, OrganismsModule, AtomsModule],
   exports: [ReactiveFormsModule, DialogModule, OrganismsModule, AtomsModule],
 })
 export class SharedModule {}
