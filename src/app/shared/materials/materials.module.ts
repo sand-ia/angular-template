7fdf46364ecacbd2f96c9f84d32ef291b68b5fb8
mikevive <mikevive327@gmail.com>
2024-03-24 18:34:31 -0500

    feat(material): remove no required module
    Co-authored-by: waltermayor <waltermayor@gmail.com>

--- src/app/shared/materials/materials.module.ts
+++ src/app/shared/materials/materials.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
@@ -31,7 +30,6 @@ import { MatDialogModule } from '@angular/material/dialog';
     CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule,
     MatCardModule,
     MatDividerModule,
     MatInputModule,
@@ -58,7 +56,6 @@ import { MatDialogModule } from '@angular/material/dialog';
   exports: [
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule,
     MatCardModule,
     MatDividerModule,
     MatInputModule,
